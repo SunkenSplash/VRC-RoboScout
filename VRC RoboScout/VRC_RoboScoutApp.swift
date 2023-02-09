76ede86e2573f4789d683ee9fef314dc043b97ba
SunkenSplash <willcas2005@icloud.com>
2023-02-09 10:08:17 -0500

    Initial Commit

---
+++ VRC RoboScout/VRC_RoboScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  VRC_RoboScoutApp.swift
+//  VRC RoboScout
+//
+//  Created by William Castro on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct VRC_RoboScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VRC RoboScoutUITests/VRC_RoboScoutUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VRC_RoboScoutUITestsLaunchTests.swift
+//  VRC RoboScoutUITests
+//
+//  Created by William Castro on 2/9/23.
+//
+
+import XCTest
+
+final class VRC_RoboScoutUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
